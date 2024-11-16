6d6b0b17c51c1457ac7cb95e8ef2e51069c1a88d
Daniel Ude <danielude61@gmail.com>
2024-11-16 13:58:02 +0100

    Added files for the assignment

---
+++ eg_db.py
@@ -0,0 +1,10 @@
+user_db = {
+    "user1": {
+        "username": "user1",
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "K4Tz3@example.com",
+        "age": 30,
+        "password": "password123",
+    }
+}
